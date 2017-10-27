ab7080a96237f8062c8b58c3a2bc47f7b4ebe857
alsandwls1 <alsandwls@gmail.com>
2017-10-27 18:48:05 +0900

    update truckRepository

---
+++ spring/src/test/java/com/example/demo/repository/TruckRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.example.demo.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TruckRepositoryTest {
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindOneById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAllByAddr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindOneByMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAvgByTruck() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateAvg() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateLocal() {
+		fail("Not yet implemented");
+	}
+
+}
